7c22a877c2e8b3f3cf9acd005ac2710e439a5c27
xw <relv@qq.com>
2019-08-08 11:34:04 +0800

    webpack react typescrit setup

---
+++ demo0808/dist/App.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var App = (function () {
+    function App() {
+        console.log("Hello app!");
+    }
+    return App;
+}());
+exports.App = App;
+//# sourceMappingURL=App.js.map
